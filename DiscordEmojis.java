7e40e655082e84b7a7f9877ad548eebcae8ca42c
ShakesPurr <31359482+ShakesPurr@users.noreply.github.com>
2017-08-25 16:15:06 -0700

    Add files via upload

---
+++ DiscordEmojis.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DiscordEmojis
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("PLease type here:");
+		String text = in.nextString();
+
+
+	}
+}
