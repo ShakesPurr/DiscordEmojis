33778c20ba059d14aef11df3e2f78e2b0942b855
ShakesPurr <31359482+ShakesPurr@users.noreply.github.com>
2017-08-25 19:22:35 -0700

    Update DiscordEmojis.java

--- DiscordEmojis.java
+++ DiscordEmojis.java
@@ -5,9 +5,57 @@ public class DiscordEmojis
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.print("PLease type here:");
-		String text = in.nextString();
+		System.out.print("PLease type here, without any spaces:");
+		String text = in.next();
+		text = text.toLowerCase();
 
-
+		for(int i = 0; i < text.length(); i++)
+		{
+			char ch = text.charAt(i);
+			if (ch == ',')
+			{
+				System.out.print(",");
+			}
+				
+			else if (ch == '.')
+			{
+				System.out.print(".");
+			}
+			
+			else if (ch == '\'')
+			{
+				System.out.print("'");
+			}
+				
+			else if (ch == '!')
+			{
+				System.out.print("!");
+			}
+				
+			else if (ch == '?')
+			{
+				System.out.print("?");
+			}
+				
+			else if (ch == '/')
+			{
+				System.out.print("/");
+			}
+				
+			else if (ch == ':')
+			{
+				System.out.print(":");
+			}
+				
+			else if (ch == ';')
+			{
+				System.out.print(";");
+			}
+				
+			else 
+			{
+					System.out.print(":regional_indicator_" + ch + ": ");	
+			}	
+		}			
 	}
 }
