4d33950d955994a086dffebac42594d3fb2a801f
ShakesPurr <31359482+ShakesPurr@users.noreply.github.com>
2017-09-01 12:27:19 -0700

    Update DiscordEmojis.java

--- DiscordEmojis.java
+++ DiscordEmojis.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class DiscordEmojis
+public class Emojis
 {
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.print("PLease type here, without any spaces:");
-		String text = in.next();
+		System.out.print("PLease type here:");
+		String text = in.nextLine();
 		text = text.toLowerCase();
 
 		for(int i = 0; i < text.length(); i++)
@@ -51,11 +51,27 @@ public class DiscordEmojis
 			{
 				System.out.print(";");
 			}
+			
+			else if (ch == 'b')
+			{
+				System.out.print(":b:");
+			}
+			
+			else if (ch == 'a')
+			{
+				System.out.print(":a:");
+			}
+			
+			else if (ch == ' ')
+			{
+				System.out.print("     ");
+			}
 				
 			else 
 			{
-					System.out.print(":regional_indicator_" + ch + ": ");	
+				System.out.print(":regional_indicator_" + ch + ": ");	
 			}	
 		}			
 	}
+	
 }
